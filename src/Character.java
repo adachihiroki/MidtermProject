1ee9bdfbf8faadba8a80cfb0eade2d0f7441daf1
AdachiHiroki <241039@std.hi-joho.ac.jp>
2025-06-23 11:40:10 +0900

    中間課題

---
+++ src/Character.java
@@ -0,0 +1,16 @@
+public abstract class Character implements Creature{
+    protected String name;
+    public int hp;
+
+    public Character(String name, int hp){
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public final boolean isAlive(){
+        return this.hp > 0;
+    }
+    public void showStatus(){
+        System.out.println(this.name + ":HP" + ' ' + this.hp);
+    }
+}
